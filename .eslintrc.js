0913fde328281adbbba0b57a686693118d4a25d6
milosz08 <gilgamilosz451@gmail.com>
2023-11-05 18:11:17 +0100

    initialized repository

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+'use strict';
+/*
+ * Copyright (c) 2023 by Visphere & Vsph Technologies
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+
+module.exports = require('../visphere-base/config/eslint/_eslint-node.config.cjs');
---
+++ README.md
@@ -0,0 +1,5 @@
+<img src="https://i.imgur.com/OGlGrFO.png" width="300px"/>
+
+# Visphere Email Processor
+
+Simple Node Express server for processing MJML email templates.
