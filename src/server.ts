6a5bf8f86d33bb334c008a4cbf47b322e2d21942
milosz08 <gilgamilosz451@gmail.com>
2023-12-13 02:42:43 +0100

    increase max payload request size

--- src/server.ts
+++ src/server.ts
@@ -14,7 +14,7 @@ const app: Express = express();
 const port = config.PORT;
 
 app.use(nocache());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 app.use(apiKeyHandlerMiddleware);
 app.use(router);
